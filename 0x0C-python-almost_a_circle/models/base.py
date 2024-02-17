9c7d64d6043ed1b0e9c6fcaeb326a322467e9f82
AdobeaAfia <adobea_afia@yahoo.com>
2024-02-16 16:45:58 -0800

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,15 +7,20 @@ import turtle
 
 class Base:
     """Base model.
+
     This Represents the "base" for all other classes in project 0x0C*.
+    
     Private Class Attributes:
     __nb_object (int): Number of instantiated Bases.
     """
+    
     __nb_objects = 0
+    
     def __init__(self, id=None):
         """Initialize a new Base.
-         Args:
-         id (int): The identity of the new Base.
+
+        Args:
+            id (int): The identity of the new Base.
          """
          if id is not None:
              self.id = id
@@ -25,36 +30,37 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-    """Return the JSON serialization of a list of dicts.
+        """Return the JSON serialization of a list of dicts.
 
-    Args:
-    list_dictionaries (list): A list of dictionaries.
+        Args:
+        list_dictionaries (list): A list of dictionaries.
     """
     if list_dictionaries is None or list_dictionaries == []:
         return "[]"
     return json.dumps(list_dictionaries)
+
     @classmethoddef
     def save_to_file(cls, list_objs):
-    """Write the JSON serialization of a list of objects to a file.
+        """Write the JSON serialization of a list of objects to a file.
+        
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
 
-    Args:
-    list_objs (list): A list of inherited Base instances.
-     """
-     filename = cls.__name__ + ".json"
-     with open(filename, "w") as jsonfile:
-         if list_objs is None:
-             jsonfile.write("[]")
-         else:
-             list_dicts = [o.to_dictionary() for o in list_objs]
-             jsonfile.write(Base.to_json_string(list_dicts))
+     @staticmethod
+     def from_json_string(json_string):
+         """Return the deserialization of a JSON string.
 
-        @staticmethod
-             def from_json_string(json_string):
-             """Return the deserialization of a JSON string.
-
-             Args:
+         Args:
              json_string (str): A JSON str representation of a list of dicts.
-             Returns:
+         Returns:
              If json_string is None or empty - an empty list.
              Otherwise - the Python list represented by json_string.
              """
