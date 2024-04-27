b12d20429064474f901dfa0cb458e96d9d23274d
AdobeaAfia <adobea_afia@yahoo.com>
2024-04-26 18:01:38 -0700

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Python script that takes in a URL, sends a request to the URL and displays the body of the response."""
 
-if __name__ == '__main__':
-    from sys import argv
-    from requests import get
+import sys
+import requests
 
-    url = argv[1]
-
-    response = get(url)
-    ERR_TXT = 'Error code: {}'
-    status = response.status_code
-    print(ERR_TXT.format(status) if (status >= 400) else response.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    body = response.text
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(body)
